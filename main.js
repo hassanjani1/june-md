93463390a328ab89a347badf1a1205120175f69d
hassanjani1 <161316228+hassanjani1@users.noreply.github.com>
2025-09-01 01:48:17 +0500

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ const videoCommand = require('./commands/video');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
+global.channelLink = "https://whatsapp.com/channel/0029VbAsXOu5fM5g9sLpV22P";
 global.ytch = "supremLord";
 
 // Add this near the top of main.js with other global configurations
