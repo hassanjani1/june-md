453ad2e648cb3a3f6967ea71d68032cf58d1ea7b
hassanjani1 <161316228+hassanjani1@users.noreply.github.com>
2025-09-01 01:44:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  packname: 'Rana-Awais🔥',
   author: 'SUPREME',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  botName: "Rana-Awais🔥",
+  botOwner: 'Rana-Awais☠️', // Your name
+  ownerNumber: '923126184997', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
